58303ca84cbf12e0203b993d86f50a59876ada78
mdwecht <mdwecht@gmail.com>
2021-09-19 22:31:15 -0400

    add more Job default field values

--- models.py
+++ models.py
@@ -5,9 +5,9 @@ class Job(models.Model):
     '''
     DSAE Fields Common to all Jobs:
     '''
-    name = models.CharField(max_length=40)
-    type = models.CharField(max_length=40)
-    state = models.CharField(max_length=40)
+    name = models.CharField(max_length=40, default='Analytic-01 Job-00')
+    type = models.CharField(max_length=40, default='AN_01')
+    state = models.CharField(max_length=40, default='IDLE')
     period = models.IntegerField(default=3600)
     last_ts = models.DateTimeField(auto_now_add=True)
     created_ts = models.DateTimeField(auto_now_add=True)
@@ -16,13 +16,13 @@ class Job(models.Model):
     '''
     DSAE Fields Specific to 'analytic_01' Job Type:
     '''
-    host = models.CharField(max_length=20)
+    host = models.CharField(max_length=20, default='funny-bunny')
     ip = models.CharField(max_length=20, default='0.0.0.0')
     ports_min = models.IntegerField(default=150)
     ports_max = models.IntegerField(default=300)
-    search = models.CharField(max_length=20)
-    sid = models.CharField(max_length=20)
-    session = models.CharField(max_length=20)
+    search = models.CharField(max_length=20, default='index=_internal')
+    sid = models.CharField(max_length=20, default='0')
+    session = models.CharField(max_length=20, default='0')
 
     def __str__(self):
         return self.name
